01e59b57d05e50636c0d26d6ba3de68c61fab4ea
Oleksii Zhyvotivskyi <gamesdashev@gmail.com>
2025-08-26 20:05:03 +0200

    Fix: підключив filtersReducer до store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/slice";
 import recipesReducer from "./recipes/slice";
-import categoriesReducer from "./categories/slice"; 
-import ingredientsReducer from "./ingredients/slice"; 
+import categoriesReducer from "./categories/slice";
+import ingredientsReducer from "./ingredients/slice";
+import filtersReducer from "./filters/slice"; // ← додай це
 import storage from "redux-persist/lib/storage";
 import {
   persistStore,
@@ -27,8 +28,9 @@ export const store = configureStore({
   reducer: {
     auth: persistedAuthReducer,
     recipes: recipesReducer,
-    categories: categoriesReducer,      
-    ingredients: ingredientsReducer,    
+    categories: categoriesReducer,
+    ingredients: ingredientsReducer,
+    filters: filtersReducer, // ← і це
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
