a93255d7b51c3bb61c2c42152d72dcd07ba31232
Ariel <arina.kudar@gmail.com>
2025-08-26 20:16:07 +0300

    review

--- src/components/LogoutButton/LogoutButton.jsx
+++ src/components/LogoutButton/LogoutButton.jsx
@@ -1,9 +1,6 @@
-import React from "react";
 import { useDispatch } from "react-redux";
+import { logOut } from "../../redux/auth/operations";
 import { useNavigate } from "react-router-dom";
-import { logout } from "../../redux/auth/slice";
-import axios from "axios";
-import styles from "./LogoutButton.module.css";
 
 const LogoutButton = () => {
   const dispatch = useDispatch();
@@ -11,24 +8,15 @@ const LogoutButton = () => {
 
   const handleLogout = async () => {
     try {
-      await axios.post("/api/auth/logout", {}, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      });
+      await dispatch(logOut()).unwrap();
     } catch (error) {
       console.error("Logout error:", error);
     } finally {
-      dispatch(logout());
-      navigate("/"); 
+      navigate("/");
     }
   };
 
-  return (
-    <button className={styles.logoutButton} onClick={handleLogout}>
-      Вийти
-    </button>
-  );
+  return <button onClick={handleLogout}>Вийти</button>;
 };
 
 export default LogoutButton;
