dadf7633af44525a94ecc2270c5ff1b70ba9050e
Alex <alexageew0506147048@gmail.com>
2025-08-26 21:22:52 +0200

    fix: resolve conflicts and wire Provider/Router

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,9 @@ import App from './App.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
