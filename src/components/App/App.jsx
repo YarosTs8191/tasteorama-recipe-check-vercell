a7d350d341554fe43b1395f0ecce42765fa36c73
outs1de <maksym.dobriden@gmail.com>
2025-08-27 22:03:20 +0300

    додав стилізацію своєї форми для всіх пристроїв, зробив спрайт

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,26 @@
-import { useEffect, lazy, Suspense } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import Layout from '../Layout/Layout';
-import PublicRoute from '../PublicRoute';
-import PrivateRoute from '../PrivateRoute';
-import { selectIsRefreshing } from '../../redux/auth/selectors';
-import { refreshUser } from '../../redux/auth/operations';
+import { useEffect, lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import Layout from "../Layout/Layout";
+import PublicRoute from "../PublicRoute";
+import PrivateRoute from "../PrivateRoute";
+import { selectIsRefreshing } from "../../redux/auth/selectors";
+import { refreshUser } from "../../redux/auth/operations";
 
-const MainPage = lazy(() => import('../../pages/MainPage/MainPage'));
-const RecipeViewPage = lazy(() => import('../../pages/RecipeViewPage/RecipeViewPage'));
-const AddRecipePage = lazy(() => import('../../pages/AddRecipePage/AddRecipePage'));
-const ProfilePage = lazy(() => import('../../pages/ProfilePage/ProfilePage'));
-const AuthPage = lazy(() => import('../../pages/AuthPage/AuthPage'));
-const RegisterPage = lazy(() => import('../../pages/AuthPage/RegisterPage'));
-const NotFound = lazy(() => import('../../components/NotFound/NotFound'));
+const MainPage = lazy(() => import("../../pages/MainPage/MainPage"));
+const RecipeViewPage = lazy(() =>
+  import("../../pages/RecipeViewPage/RecipeViewPage")
+);
+const AddRecipePage = lazy(() =>
+  import("../../pages/AddRecipePage/AddRecipePage")
+);
+const ProfilePage = lazy(() => import("../../pages/ProfilePage/ProfilePage"));
+const AuthPage = lazy(() => import("../../pages/AuthPage/AuthPage"));
+const RegisterPage = lazy(() => import("../../pages/AuthPage/RegisterPage"));
+const NotFound = lazy(() => import("../../components/NotFound/NotFound"));
 
 function App() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const isRefreshing = useSelector(selectIsRefreshing);
 
   useEffect(() => {
@@ -32,40 +36,39 @@ function App() {
       <Routes>
         {/* Загальний Layout */}
         <Route path="/" element={<Layout />}>
-
           {/* Публічні маршрути */}
-          <Route 
-            index 
+          <Route
+            index
             element={
               <PublicRoute restricted={false}>
                 <MainPage />
               </PublicRoute>
-            } 
+            }
           />
-          <Route 
-            path="recipes/:recipeId" 
+          <Route
+            path="recipes/:recipeId"
             element={
               <PublicRoute restricted={false}>
                 <RecipeViewPage />
               </PublicRoute>
-            } 
+            }
           />
-          <Route 
-            path="auth/login" 
+          <Route
+            path="auth/login"
             element={
               <PublicRoute restricted={true}>
                 <AuthPage />
               </PublicRoute>
-            } 
+            }
           />
 
-          <Route 
-            path="auth/register" 
+          <Route
+            path="auth/register"
             element={
               <PublicRoute restricted={true}>
                 <RegisterPage />
               </PublicRoute>
-            } 
+            }
           />
 
           {/* Приватні маршрути */}
@@ -78,7 +81,6 @@ function App() {
 
           {/* Catch all 404 */}
           <Route path="*" element={<NotFound />} />
-
         </Route>
       </Routes>
     </Suspense>
