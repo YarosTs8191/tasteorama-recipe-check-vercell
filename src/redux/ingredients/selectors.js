782e0b1b99baa1b1c7dacf9d3b4a99892b3107e7
Ariel <arina.kudar@gmail.com>
2025-08-27 21:07:05 +0300

    Revert "Main page"

--- src/redux/ingredients/selectors.js
+++
@@ -1,4 +0,0 @@
-export const selectIngredients = ({ ingredients }) => ingredients.items;
-export const selectIngredientsIsLoading = ({ ingredients }) =>
-  ingredients.isLoading;
-export const selectIngredientsError = ({ ingredients }) => ingredients.error;
