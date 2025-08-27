bec8c4d0b9f65fa1ee30e708cee5c25dfef45d87
Valentyna <valia.melnik@gmail.com>
2025-08-27 19:50:56 +0200

    footer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./auth/slice";
-import recipesReducer from "./recipes/slice";
-import categoriesReducer from "./categories/slice";
-import ingredientsReducer from "./ingredients/slice";
-import filtersReducer from "./filters/slice"; // ← додай це
-import storage from "redux-persist/lib/storage";
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './auth/slice';
+import recipesReducer from './recipes/slice';
+import categoriesReducer from './categories/slice';
+import ingredientsReducer from './ingredients/slice';
+import filtersReducer from './filters/slice'; // ← додай це
+import modal from './modal/slice';
+import storage from 'redux-persist/lib/storage';
 import {
   persistStore,
   persistReducer,
@@ -14,12 +15,12 @@ import {
   PERSIST,
   PURGE,
   REGISTER,
-} from "redux-persist";
+} from 'redux-persist';
 
 const authPersistConfig = {
-  key: "auth",
+  key: 'auth',
   storage,
-  whitelist: ["token"],
+  whitelist: ['token'],
 };
 
 const persistedAuthReducer = persistReducer(authPersistConfig, authReducer);
@@ -31,8 +32,9 @@ export const store = configureStore({
     categories: categoriesReducer,
     ingredients: ingredientsReducer,
     filters: filtersReducer, // ← і це
+    modal,
   },
-  middleware: (getDefaultMiddleware) =>
+  middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
