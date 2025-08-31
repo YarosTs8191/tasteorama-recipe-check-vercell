922e23cf7370fc6c3596ef1b4c3e3fce0b960e40
Oleh_Denysenko <Oleh.Denysenkoo@gmail.com>
2025-08-31 11:34:34 +0300

    add mobile first styles

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -3,8 +3,6 @@ import LoginForm from "../../components/LoginForm/LoginForm";
 export default function LoginPage() {
   return (
     <div>
-      <h1>My profile</h1>
-      {/* <ProfileNavigation /> */}
       <LoginForm />
     </div>
   );
