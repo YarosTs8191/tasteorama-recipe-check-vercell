1e712a686f868e7fb320430544b438b429591b07
YarosTs8191 <143120910+YarosTs8191@users.noreply.github.com>
2025-09-01 14:08:37 +0200

    add notfound.png

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./NotFound.module.css";
-import notFoundImg from "../../assets/images/notfound.png";
+import notFoundImg from "../../assets/images/notFoundImg.jpg";
 
 const NotFound = () => {
   const navigate = useNavigate();
